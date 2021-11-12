d794e788cc63ab04cf0ba8708b65b27e87938e51
rpizarrog <rpizarro@itdurango.edu.mx>
2021-11-12 06:04:40 -0600

    Create combinaciones.R

--- scripts/combinaciones.R
+++ scripts/combinaciones.R
@@ -7,3 +7,8 @@ k <- 2 # combinar de dos en dos
 
 combinaciones <- combinations(n = n, r = k, v = ingredientes)
 combinaciones
+
+# permutaciones
+# Si importa el orden en que se agreguen los ingredientes por aquello de los sabores
+permutaciones <- permutations(n = n, r = k, v = ingredientes)
+permutaciones
