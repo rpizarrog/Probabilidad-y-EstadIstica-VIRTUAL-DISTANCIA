497b417baa853a1cce25b1c210044f5c737fe9e4
rpizarrog <rpizarro@itdurango.edu.mx>
2021-12-02 12:49:56 -0600

    Update f.diagramas.graficos.r

--- funciones/f.diagramas.graficos.r
+++ funciones/f.diagramas.graficos.r
@@ -2,19 +2,23 @@
 
 # Construye diagrama de dispersión 
 f_diag.dispersion <- function (datos) { 
-  #datos <- data.frame(datos)
+  # datos <- data.frame(datos)
   nom.x = colnames(datos[1])
   nom.y = colnames(datos[2])
+  x = datos[,1]
+  y = datos[,2]
+  
+  media.x <- round(mean(x), 4)
+  media.y <- round(mean(y), 4)
   
   ggplot() +
-    geom_point(aes(x = datos[,1], y = datos[,2]), col='red') +
-    geom_vline(xintercept = mean(datos[,1]), col='blue') +
-    geom_hline(yintercept = mean(datos[,2]), col='blue') +
+    geom_point(aes(x = x, y = y), col='red') +
+    geom_vline(xintercept = media.x, col='blue') +
+    geom_hline(yintercept = media.y, col='blue') +
     ggtitle(label = paste("Dispersión de ", nom.x, " y ", nom.y) , 
-            subtitle = paste("Media ", nom.x, " =", mean(datos[,1]), " , ", "Media ", nom.y, "=", mean(datos[,2])))
+            subtitle = paste("Media ", nom.x, " =", media.x, 
+                             " , ", "Media ", nom.y, "=", media.y))+
+    xlab( nom.x)+
+    ylab( nom.y)
+  
 }
-
-
-
-
-
