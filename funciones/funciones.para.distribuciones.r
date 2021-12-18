af4e0796ebf786d95bc26466d9e11372475c775f
rpizarrog <rpizarro@itdurango.edu.mx>
2021-12-18 06:05:28 -0600

    Add files via upload

--- funciones/funciones.para.distribuciones.r
+++ funciones/funciones.para.distribuciones.r
@@ -134,9 +134,14 @@ f.devolver.z <- function(x, media, desv) {
   z
 }
 
-f.devolver.z.prueba <- function(media.m, desv.m, media.p, n) {
-  z <- (media.m - media.p) / (desv.m / sqrt(n))
+f.devolver.z.prueba <- function(media.m, desv.p, media.p, n) {
+  z <- (media.m - media.p) / (desv.p / sqrt(n))
   z
+}
+
+f.devolver.t.prueba <- function(media.m, desv.m, media.p, n) {
+  t <- (media.m - media.p) / (desv.m / sqrt(n))
+  t
 }
 
 
